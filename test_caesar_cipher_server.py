6ed5321668b00005d3174d73b87c0b0189b3b917
Greg Mojonnier <gregmojonnier@gmail.com>
2016-09-12 23:14:52 -0400

    Swapped out python's default unittest for py.test.

--- test_caesar_cipher_server.py
+++ test_caesar_cipher_server.py
@@ -1,63 +1,70 @@
 import socket
 import select
-import unittest
 import time
+import pytest
 
 
-class CaesarCipherServerTests(unittest.TestCase):
-    _host = '127.0.0.1'
-    _server_port = 55555
-    _sock = None
+@pytest.fixture
+def CaesarCipherServerFixture(request):
+    class TestInfo:
+        _host = '127.0.0.1'
+        _server_port = 55555
+        _sock = None
 
-    def setUp(self):
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        def __init__(self):
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self._sock.connect((self._host, self._server_port))
+            except Exception as e:
+                assert False, 'Unable to connect to server on port ' + \
+                    str(self._server_port) + ', exception: ' + str(e)
 
-    def tearDown(self):
-        self._sock.shutdown(socket.SHUT_RDWR)
-        self._sock.close()
-        self._sock = None
+    t = TestInfo()
 
-    def connect_to_server(self):
-        try:
-            self._sock.connect((self._host, self._server_port))
-        except Exception as e:
-            self.fail('Unable to connect to server on port ' +
-                      str(self._server_port) + ', exception: ' + str(e))
+    def Uninitialize():
+        t._sock.shutdown(socket.SHUT_RDWR)
+        t._sock.close()
+        t._sock = None
 
-    def test_ServerIsRunning_AConnectionIsEstablished(self):
-        self.connect_to_server()
+    request.addfinalizer(Uninitialize)
+    return t
 
-    def test_ClientSendsTextWithoutTrailingSpace_ServerWillWaitForAdditionalMessages(self):
-        self.connect_to_server()
-        two_second_timeout = 2
-        timed_out = False
 
-        self._sock.sendall(b'text_without_a_trailing_space')
+def test_ClientSendsTextWithosdfutTrailingSpace_ServerWillWaitForAdditionalMessages(CaesarCipherServerFixture):
+    sock = CaesarCipherServerFixture._sock
+    two_second_timeout = 2
+    timed_out = False
 
-        self._sock.setblocking(0)
-        ready = select.select([self._sock], [], [], two_second_timeout)
-        if not ready[0]:
-            timed_out = True
-        self.assertTrue(timed_out)
+    sock.sendall(b'text_without_a_trailing_space')
+    sock.setblocking(0)
+    ready = select.select([sock], [], [], two_second_timeout)
+    if not ready[0]:
+        timed_out = True
 
-    def test_ClientsFirstWordIsNotANumber_ServerWontRespondAndClosesConnection(self):
-        self.connect_to_server()
-        response = None
-        try:
-            self._sock.sendall('Not_A_Number message'.encode())
-            response = self._sock.recv(1024)
-            self.assertFalse(response)
-        except socket.error, e:
-            self.fail(
-                'Socket error ' + str(e))
+    assert timed_out
 
-    def test_ClientsFirstWordIsANumber_ServerRespondsWithNonEmptyMessageAndKeepsConnectionOpen(self):
-        self.connect_to_server()
-        try:
-            self._sock.sendall('32 message'.encode())
-            response = self._sock.recv(1024)
-            self.assertTrue(response)
-        except socket.error, e:
-            print('sock error ', e)
-            self.fail(
-                'Connection was closed even though a number was sent for the first word.')
+
+def test_ClientsFirstWordIsNotANumber_ServerClosesConnection(CaesarCipherServerFixture):
+    sock = CaesarCipherServerFixture._sock
+    response = None
+
+    try:
+        sock.sendall(b'Not_A_Number message')
+        response = sock.recv(1024)
+    except socket.error, e:
+        pass
+
+    assert response == ''
+
+
+def test_ClientsFirstWordIsANumber_ServerRespondsWithMessage(CaesarCipherServerFixture):
+    sock = CaesarCipherServerFixture._sock
+    response = None
+
+    try:
+        sock.sendall(b'32 message')
+        response = sock.recv(1024)
+    except socket.error, e:
+        pass
+
+    assert response
